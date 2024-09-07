058b47ef2c52348efb494e41651a68128a35cddc
Anup Gorai <anupgorai51056@gmail.com>
2024-09-07 21:45:55 +0530

    first commit

---
+++ src/main/java/com/mario/deleteEmployeeServlet.java
@@ -0,0 +1,46 @@
+package com.mario;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class deleteEmployeeServlet
+ */
+@WebServlet("/deleteEmployeeServlet")
+public class deleteEmployeeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public deleteEmployeeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		int eno = Integer.parseInt(request.getParameter("eno"));
+       
+
+        EmployeeDAO.deleteEmployee(eno);
+
+        response.sendRedirect("index.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
